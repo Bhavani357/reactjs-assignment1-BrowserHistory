38a54fc8a76e4fa2be9910464ec862a656dcb974
durgabhavani <durgabhavaniyasarla232@gmail.com>
2023-04-24 01:52:32 +0000

    successfully completed

--- src/SearchHistoryItem/index.js
+++ src/SearchHistoryItem/index.js
@@ -12,21 +12,24 @@ const SearchHistoryItem = props => {
       <p className="time">{timeAccessed}</p>
       <div className="li-items-container">
         <img src={logoUrl} alt="domain logo" className="logo" />
-        <p className="span">{title}</p>
-        <p>{domainUrl}</p>
-
-        <button
-          type="button"
-          className="button"
-          data-testid="delete"
-          onClick={onDeleteItem}
-        >
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
-            alt="delete"
-            className="delete-button"
-          />
-        </button>
+        <div className="paras-container">
+          <p className="span">{title}</p>
+          <p className="paragraph">{domainUrl}</p>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="button"
+            data-testid="delete"
+            onClick={onDeleteItem}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+              alt="delete"
+              className="delete-button"
+            />
+          </button>
+        </div>
       </div>
     </li>
   )
